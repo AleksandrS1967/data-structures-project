82fa9994ca586a307859094408b18becb01b78f0
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-01-24 15:54:30 +0300

    Добавил 2-а класса и два метода + тесты

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+
+import unittest
+
+from src.stack import Stack
+stack = Stack()
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        stack.push('data_1')
+        self.assertEqual(stack.top.data, 'data_1')
+
+    def test_pop(self):
+        self.assertEqual(stack.pop(), None)
+        stack.push('data_1')
+        stack.push('data_2')
+        stack.pop()
+        self.assertEqual(stack.pop(), 'data_1')
